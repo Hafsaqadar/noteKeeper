b53fd7993ab7094e3bd22867c8d5537cc5d06a89
hafsaqadar <haf252537@gmail.com>
2025-07-08 18:19:58 +0300

    updated

--- src/component/Sidebar.tsx
+++ src/component/Sidebar.tsx
@@ -1,7 +1,19 @@
 import { HiPlusSm } from "react-icons/hi";
+ import NewNotebookForm from "./NewNotebookForm";
 
 
-const Sidebar = () => {
+
+ type Notebook = {id: string;  value: string; saved: boolean };
+
+type Props = {
+  notebooks: Notebook[];
+  setNotebooks: React.Dispatch<React.SetStateAction<Notebook[]>>;
+};
+
+
+const Sidebar = ({ notebooks, setNotebooks }: Props) => {
+
+
   return (
     <div className="w-60 h-full bg-orange-100 text-white p-4">
    
@@ -9,13 +21,10 @@ const Sidebar = () => {
      <button className="mt-5 px-3 py-2 bg-orange-200 text-gray-600 font-semibold rounded-lg  hover:bg-gray-200 flex items-center gap-1">
       <HiPlusSm size={20} className="mt-1"  />  new note
         </button>
+        <NewNotebookForm  notebooks={notebooks} setNotebooks={setNotebooks}  />
 
-     <h2 className="mt-2 text-gray-600 font-semibold flex items-center gap-20">NoteBooks
-        <HiPlusSm size={20} className="mt-1"   /> 
-     </h2>
+    
      
- 
-   
 
   
     </div>
